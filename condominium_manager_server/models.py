c46c04218d98c079dfd8c519acbc309d9d244a69
Luan Lima <luanlima.olliveir@gmail.com>
2021-01-13 21:36:36 -0300

    Create model proprietor.

---
+++ condominium_manager_server/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Proprietor(models.Model):
+    name = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=11)
+    block = models.IntegerField()
+    lot = models.IntegerField()
+    principal_phone = models.CharField(max_length=14)
+    secondary_phone = models.CharField(max_length=14)
+
+    def __str__(self):
+        return f'Proprietor: {self.id} - {self.name} - {self.cpf}'
