6a4a3fa41197fd8e2ab84fab346a8be516739af9
Luan Lima <luanlima.olliveir@gmail.com>
2021-01-14 21:03:32 -0300

    Create model reservation shift.

--- condominium_manager_server/models.py
+++ condominium_manager_server/models.py
@@ -10,3 +10,9 @@ class Proprietor(models.Model):
 
     def __str__(self):
         return f'Proprietor: {self.id} - {self.name} - {self.cpf}'
+
+class ReservationShift(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f'Reservation shift: {self.id} - {self.name}'
