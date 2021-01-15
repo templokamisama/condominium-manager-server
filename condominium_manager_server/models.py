ffce5815180dc4f72073a0eeef7d40ba94c09d32
Luan Lima <luanlima.olliveir@gmail.com>
2021-01-14 21:07:16 -0300

    Create model reservation states.

--- condominium_manager_server/models.py
+++ condominium_manager_server/models.py
@@ -12,7 +12,13 @@ class Proprietor(models.Model):
         return f'Proprietor: {self.id} - {self.name} - {self.cpf}'
 
 class ReservationShift(models.Model):
-    name = models.CharField(max_length=50)
-    
+    name = models.CharField(max_length=20)
+
     def __str__(self):
         return f'Reservation shift: {self.id} - {self.name}'
+
+class ReservationStates(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'Reservation states: {self.id} - {self.name}'
